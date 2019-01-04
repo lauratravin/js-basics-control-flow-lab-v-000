0e066d610e5f0376fcf84d14392bd1272446b56a
Laura Messa Travin <laura.travin@gmail.com>
2019-01-04 22:33:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue){
+ let message
+  if (someValue > 2500) {
+         message = 'No can do.';
+  }
+  else {
+     if (someValue > 2000){
+           message = 'I will gladly take your thirty bucks.';
+          }
+     else  {
+            if (someValue<= 400) {
+              message = 'This one is on me!';
+            }    
+     }    
+  }          
+}
